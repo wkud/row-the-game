2ab3ee500017d9397dbe66426973a565b3501f16
Jakub Plona <plonajakub@gmail.com>
2020-06-26 16:51:20 +0200

    Add player prototype

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float moveSpeed = 3.0f;
+
+    Rigidbody m_Rigidbody;
+    Vector3 m_Movement;
+
+    void Start()
+    {
+        m_Rigidbody = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        m_Movement.Set(horizontal, 0f, vertical);
+        m_Movement.Normalize();
+
+        m_Rigidbody.MovePosition(m_Rigidbody.position + moveSpeed * m_Movement * Time.deltaTime);
+    }
+}
