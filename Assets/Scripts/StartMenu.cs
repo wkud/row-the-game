d6828909e632df14506430489f38d302b2eda15c
Jakub Plona <plonajakub@gmail.com>
2020-06-28 14:22:04 +0200

    Add basic UI: start menu and game over menu

---
+++ Assets/Scripts/StartMenu.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartMenu : MonoBehaviour
+{
+    public void StartGame()
+    {
+        gameObject.SetActive(false);
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("QuitGame() called");
+        Application.Quit();
+    }
+}
