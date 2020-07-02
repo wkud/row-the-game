302389a9cc2e940796a503a832d0de7546a5cef0
Wojtek <vopox07@gmail.com>
2020-07-02 14:20:45 +0200

    Move camera instead of river

---
+++ Assets/Scripts/Camera.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera : MonoBehaviour
+{
+    [SerializeField] private float flowSpeed;
+
+    void FixedUpdate()
+    {
+        transform.position += Vector3.forward * flowSpeed;
+    }
+}
